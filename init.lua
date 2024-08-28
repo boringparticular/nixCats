cf12b4f67a185fd0cd7c0f5c37240c7c68f93eb2
Kai Mies <develop@regnavi.de>
2024-08-28 11:17:25 +0200

    feat: add harpoon

--- init.lua
+++ init.lua
@@ -1094,6 +1094,41 @@ require('nixCatsUtils.lazyCat').setup(pluginList, nixLazyPath, {
         ft = { 'go', 'gomod' },
         build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
+    {
+        'ThePrimeagen/harpoon',
+        branch = 'harpoon2',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local harpoon = require('harpoon')
+
+            harpoon:setup()
+
+            -- basic telescope configuration
+            local conf = require('telescope.config').values
+            local function toggle_telescope(harpoon_files)
+                local file_paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(file_paths, item.value)
+                end
+
+                require('telescope.pickers')
+                    .new({}, {
+                        prompt_title = 'Harpoon',
+                        finder = require('telescope.finders').new_table({
+                            results = file_paths,
+                        }),
+                        previewer = conf.file_previewer({}),
+                        sorter = conf.generic_sorter({}),
+                    })
+                    :find()
+            end
+
+            -- stylua: ignore
+            vim.keymap.set('n', '<C-e>', function() toggle_telescope(harpoon:list()) end, { desc = 'Open harpoon window' })
+            -- stylua: ignore
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = 'Harpoon [A]dd' })
+        end,
+    },
 
     -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
