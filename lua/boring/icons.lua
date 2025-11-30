5f1cb853230ef578cf930eebcc4b63971244ecd9
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-11-30 05:38:16 +0100

    feat: add icons

---
+++ lua/boring/icons.lua
@@ -0,0 +1,38 @@
+local M = {
+    diagnostics = {
+        error = '',
+        warn = '',
+        info = '',
+        hint = '',
+    },
+    debug = '',
+    git = '',
+    search = '',
+    ai = '',
+    code = '',
+    toggle_off = '',
+    toggle_on = '',
+    dap = {
+        pause = '',
+        play = '',
+        step_into = '',
+        step_over = '',
+        step_out = '',
+        step_back = '',
+        run_last = '',
+        terminate = '',
+        disconnect = '',
+
+        expanded = '',
+        collapsed = '',
+        current_frame = '',
+
+        breakpoint = '',
+        breakpoint_condition = '',
+        log_point = '',
+        stopped = '',
+        breakpoint_rejected = '',
+    },
+}
+
+return M
