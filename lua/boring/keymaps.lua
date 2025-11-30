6213c94add6c86ede7775ad3f2da736fa816ea88
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-11-30 05:38:16 +0100

    feat(debug): add clue

--- lua/boring/keymaps.lua
+++ lua/boring/keymaps.lua
@@ -22,6 +22,7 @@ nmap(']p', '<Cmd>exe "iput "  . v:register<CR>', 'Paste Below')
 -- Add an entry if you create a new group.
 _G.Config.leader_group_clues = {
   { mode = 'n', keys = '<Leader>b', desc = '+Buffer' },
+  { mode = 'n', keys = '<Leader>d', desc = '+Debug' },
   { mode = 'n', keys = '<Leader>e', desc = '+Explore/Edit' },
   { mode = 'n', keys = '<Leader>f', desc = '+Find' },
   { mode = 'n', keys = '<Leader>g', desc = '+Git' },
